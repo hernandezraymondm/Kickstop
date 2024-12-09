66ad9aa0fb2c6a0b179980b768b985e8fabe50e8
Ray Hernandez <107069990+hernandezraymondm@users.noreply.github.com>
2024-12-09 08:18:01 +0800

    Move readme file

--- frontend/src/components/Loader/LoadingDots.jsx
+++ frontend/src/components/Loader/LoadingDots.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const LoadingDots = ({ size }) => {
+const LoadingDots = ({ size = 'loading-md' }) => {
   return (
     <div className="flex justify-center items-center">
       <span className={`loading loading-dots ${size}`}></span>
     </div>
   );
+};
+
+// Prop validation for better clarity and error detection
+LoadingDots.propTypes = {
+  size: PropTypes.oneOf([
+    'loading-xs',
+    'loading-sm',
+    'loading-md',
+    'loading-lg',
+  ]),
+};
+
+// Default props
+LoadingDots.defaultProps = {
+  size: 'loading-md',
 };
 
 export default LoadingDots;
